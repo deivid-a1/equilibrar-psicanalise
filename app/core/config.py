4a16a04cd0312b68d1df80b4056445d3b1bc6296
Deivid Carvalho <deivid_506_02@hotmail.com>
2026-03-30 13:55:42 -0300

    update: h 60 logo

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,8 @@ class Settings(BaseSettings):
     smtp_password: str
     contact_email: str = "equilibrarpsicanalise@gmail.com"
 
+    server_port : int = 8000
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.api.routers import router
 from app.core.logger import setup_logging
+from app.core.config import settings
 from loguru import logger
 
 os.makedirs("logs", exist_ok=True)
@@ -14,3 +16,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router)
 
 logger.info("Aplicação Equilibrar Psicanálise inicializada com sucesso")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=settings.server_port)
